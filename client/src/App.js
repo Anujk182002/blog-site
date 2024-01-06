0072d8607fefe30acced3f43b68e31084404c1e4
Anuj Kumar <anujk182002@gmail.com>
2024-01-06 14:45:07 +0530

    components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,49 @@
 
 import './App.css';
+import Register from './pages/Register' 
+import Login from './pages/Login'
+import Home from './pages/Home' 
+import Single from './pages/Single' 
+import Write from './pages/Write'  
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/Home",
+    element: <div>
+    <Navbar/>
+    <Home/>
+    <Footer/>
+    </div>
+  },
+
+  {
+    path: "/Login",
+    element: <Login/>,
+  },
+  {
+    path: "/Register",
+    element: <Register/>,
+  },
+  {
+    path: "/Single",
+    element: <Single/>,
+  },
+  {
+    path: "/Write",
+    element: <Write/>,
+  },
+]);
 
 function App() {
   return (
     <div >
-      blog site 
+       <RouterProvider router={router} />
     </div>
   );
 }
